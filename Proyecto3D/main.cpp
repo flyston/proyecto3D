cb2a967bcc42044784a46e5150c8ee1aa734f1e3
Iván Velazquez Reyes <ivan@macbook-pro-de-ivan.local>
2018-05-20 11:49:38 -0500

    Initial Commit

---
+++ Proyecto3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proyecto3D
+//
+//  Created by Iván Velazquez Reyes on 20/05/18.
+//  Copyright © 2018 Iván Velazquez Reyes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
